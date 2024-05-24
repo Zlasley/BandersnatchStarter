3c8490e36fdcfa60ca273b82b125d551d9f25d20
Zachary Lasley <116044492+Zlasley@users.noreply.github.com>
2024-05-23 20:38:54 -0500

    Update data.py

--- app/data.py
+++ app/data.py
@@ -5,7 +5,7 @@ from MonsterLab import Monster
 from pandas import DataFrame
 from pymongo import MongoClient
 
-
+a
 class Database:
     """Class that holds a MongoDB database and collection of monster objects."""
 
